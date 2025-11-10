208dfa5b0af4ed3486e159d292cbe1953bae742d
IsraelYarbloom <i0504170922@gmail.com>
2025-11-10 14:55:29 +0200

    added user file

---
+++ library/user.py
@@ -0,0 +1,23 @@
+class User:
+    def __init__(self, name, last_ID_added=-1):
+        self. name = name
+        self.id = last_ID_added + 1
+        self.borrowed_books = []
+        # self.book_lending_history = []
+    
+    def borrow_books(self, *books):
+        for book in books:
+            self.borrowed_books.append(book)
+    #        self.book_lending_history.append(book for book in books)
+
+    def __str__(self):
+        return f"\nUSER DETAILS: \nNAME: {self.name} \nID; {self.id} \nBORROWED BOOKS: {self.borrowed_books}\n"
+
+    def get_info(self):
+        return self.__str__()
+
+
+# a1 = User("israel")
+# a1.borrow_books("aaa", "bbb", "ccc")
+# print(a1)
+# print(a1.get_info())
