f6f7e9eced140d16f0dcef06d5c91317286efbc5
EfraimGottlieb <efgo583208979@gmail.com>
2025-11-11 12:06:13 +0200

    ready to merge

--- library/library.py
+++ library/library.py
@@ -14,17 +14,23 @@ class Library:
       JsonHandeling.write_json(file)
 
   def __init__(self, books_file =BOOKS_PATH_FILE, users_file =USERS_PATH_FILE):
-    self.books = JsonHandeling.read_json(books_file)
-    self.users =  JsonHandeling.read_json(users_file)
+    self.books_file = books_file
+    self.users_file = users_file
+
+  def get_books_list(self):
+      return JsonHandeling.read_json(self.books_file)
+
+  def get_users_list(self):
+      return JsonHandeling.read_json(self.users_file)
 
   def add_book(self, book):
-    books = self.books
+    books = self.get_users_list()
     books[book.ISBN] = vars(book)
     JsonHandeling.write_json(BOOKS_PATH_FILE, books)
 
   def add_user(self, name, id):
     user = User(name, id)
-    users = self.users
+    users = self.get_users_list()
     if not str(user.id) in users.keys():
       users[user.id] = vars(user)
       JsonHandeling.write_json(USERS_PATH_FILE, users)
@@ -32,10 +38,11 @@ class Library:
   def borrow_book(self, user_id, book_isbn):
     users = self.get_users_list()
     books = self.get_books_list()
-    users[user_id]['borrowed_books'].append(books[book_isbn])
-    JsonHandeling.write_json(USERS_PATH_FILE, users)
-    books[book_isbn]['is_available'] = False
-    JsonHandeling.write_json(BOOKS_PATH_FILE, books)
+    if books[book_isbn]['is_available']:
+      users[user_id]['borrowed_books'].append(books[book_isbn])
+      JsonHandeling.write_json(USERS_PATH_FILE, users)
+      books[book_isbn]['is_available'] = False
+      JsonHandeling.write_json(BOOKS_PATH_FILE, books)
 
   def return_book(self, user_id, book_isbn):
     users = self.get_users_list()
@@ -47,13 +54,11 @@ class Library:
     books[book_isbn]['is_available'] = True
     JsonHandeling.write_json(BOOKS_PATH_FILE, books)
   
-  def list_available_books(self):
-    with open(self.books_list, 'r') as books_file:
-      books = json.load(books_file)
-    return [book['title'] for k,book in books.items() if book['is_available']]
+  def list_available_books(self): 
+    return [book['title'] for k,book in self.books.items() if book['is_available']]
   # search by 'title', or 'author'
   def search_book(self, search_by, search_value):
-    books = self.get_books_list()
+    books = self.books
     if search_by == 'title':
       return [{book['title'], book['author']} for k,book in books.items() if search_value.lower() in book['title'].lower()]
     elif search_by == 'author':
@@ -62,5 +67,7 @@ class Library:
 
 if __name__ == "__main__":
   b = Library()
-  b.add_book(Book('mishna', '123'))
+
   b.add_user('efraim', '326080025')
+  # b.borrow_book('326080025', '1009')
+  b.return_book('326080025', '1009')
